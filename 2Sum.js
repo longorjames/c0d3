2f8b26f467c4591695e054e9399acc6fab530e21
Long Vu <jakemyster2009@yahoo.com>
2020-11-05 19:52:56 -0800

    Update 2Sum.js
    Add console log test lines

--- 2Sum.js
+++ 2Sum.js
@@ -1,7 +1,11 @@
 num1 = 0 //num1 is a variable which stores the number 0
 num2 = 0 //num2 is a variable which stores the number 0
+console.log(num1) //test initial num1 value
+console.log(num2) //test inicial num2 value
 
 const sum = new Function('num1', 'num2', 'return num1 + num2') //should add num1 and num2 together
 let solution = sum(5, 9)
+console.log(solution) //should equal 14
 
 solution = sum(4,1)//should equal 5
+console.log(solution)
