d8153279765fdba445976064e47039993a5ee72f
Long Vu <jakemyster2009@yahoo.com>
2020-11-05 19:40:11 -0800

    Update 2Sum.js
    add line for readability

--- 2Sum.js
+++ 2Sum.js
@@ -1,5 +1,6 @@
 num1 = 0 //num1 is a variable which stores the number 0
 num2 = 0 //num2 is a variable which stores the number 0
+
 const sum = new Function('num1', 'num2', 'return num1 + num2') //should add num1 and num2 together
 let solution = sum(5, 9)
 
